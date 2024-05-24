d33698002b0ac865ddea3401d313be11634df699
PRANJAL KAR <74347116+pranjalkar99@users.noreply.github.com>
2024-05-24 22:13:54 +0530

    Update grok-api.py
    Updated Now

--- grok-api.py
+++ grok-api.py
@@ -33,44 +33,48 @@ class SearchParameter(BaseModel):
 
 @app.post("/analyze", response_class=StreamingResponse)
 async def analyze(search_parameter: SearchParameter):
-    start_time = time.time()
-    # Call the ChatGroq API
-    chat = ChatGroq(
-        temperature=0,
-        groq_api_key=GROQ_API_KEY,
-        model_name="llama3-8b-8192",
-        streaming=True,
-    )
-    system = """
-You are  an experienced legal research assistant, you are tasked with analyzing 
-how the provided "context" is related to the search parameter "search_parameter" 
-Your task is to write  very concise 3 bullets outlining how the "context" is related to the search parameter, so that your boss can easily understand if "context" is worth spending time. Include facts, names, dates, and other relevant information, in your answer, so that your boss can have most useful facts, and reasons at once. Keep the reasons very concise, and to the point, so that your boss can quickly understand the relevance of the context to the search parameter, but it should be informative enough to give a clear picture of the context.
+    try:
+        start_time = time.time()
+        # Call the ChatGroq API
+        chat = ChatGroq(
+            temperature=0,
+            groq_api_key=GROQ_API_KEY,
+            model_name="llama3-8b-8192",
+            streaming=True,
+        )
+        system = """
+    You are  an experienced legal research assistant, you are tasked with analyzing 
+    how the provided "context" is related to the search parameter "search_parameter" 
+    Your task is to write  a short,concise paragraphs (with atleast three sentences) outlining how the "context" is related to the search parameter, so that your boss can easily understand if "context" is worth spending time. Include facts, names, dates, and other relevant information, in your answer, so that your boss can have most useful facts, and reasons at once. Keep the reasons very concise, and to the point, so that your boss can quickly understand the relevance of the context to the search parameter, but it should be informative enough to give a clear picture of the context. Return a list of single paragraph answer.
 
-    """
-    human = f"The search parameter is {search_parameter.search_parameter} and the context is {search_parameter.context} Analyze carefully, and write how the context is related to the search parameter"
-    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+        """
+        human = f"The search parameter is {search_parameter.search_parameter} and the context is {search_parameter.context} Analyze carefully, and write how the context is related to the search parameter"
+        prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
-    chain = prompt | chat
-    chunks = ""
+        chain = prompt | chat
+        # chunks = ""
 
-    # async def stream_chunks():
-    #     async for chunk in chain.stream({"search_parameter": search_parameter.search_parameter, "context": search_parameter.context}):
-    #         yield jsonable_encoder(chunk.content)
+        # # async def stream_chunks():
+        # #     async for chunk in chain.stream({"search_parameter": search_parameter.search_parameter, "context": search_parameter.context}):
+        # #         yield jsonable_encoder(chunk.content)
 
-    # return StreamingResponse(stream_chunks(), media_type="application/json")
+        # # return StreamingResponse(stream_chunks(), media_type="application/json")
 
-    for chunk in chain.stream(
-        {
-            "search_parameter": search_parameter.search_parameter,
-            "context": search_parameter.context,
-        }
-    ):
-        chunks += (jsonable_encoder(chunk.content))
+        # for chunk in chain.stream(
+        #     {
+        #         "search_parameter": search_parameter.search_parameter,
+        #         "context": search_parameter.context,
+        #     }
+        # ):
+        #     chunks += (jsonable_encoder(chunk.content))
 
-    end_time = time.time()
-    print(f"Time taken: {end_time-start_time}")
-    return Response(content=json.dumps(chunks), media_type="application/json")
-
+        res = chain.invoke({"text": search_parameter.search_parameter, "context": search_parameter.context})
+        end_time = time.time()
+        print(f"Time taken: {end_time-start_time}")
+        answer = res.dict()['content'].split(":\n\n")[-1]
+        return Response(content=json.dumps([answer]), media_type="application/json")
+    except Exception as e:
+        return Response(content=[], media_type="application/json")
 
 if __name__ == "__main__":
     import uvicorn
